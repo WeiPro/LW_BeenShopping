aec449c45090397b63206a72798fd8c016d708da
WeiPro <keyweiqq.991@qq.com>
2016-10-30 21:54:10 +0800

    项目初始化

---
+++ LW_BeenShopping/LW_BeenShopping/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LW_BeenShopping
+//
+//  Created by wei on 2016/10/30.
+//  Copyright © 2016年 LWSpro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
